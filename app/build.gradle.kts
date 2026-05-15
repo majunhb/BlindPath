a5e1048d689d233db60a87d7ccccf48c6aeae546
majunhb <bojizhang@qq.com>
2026-05-15 17:28:50 +0800

    Update KSP plugin version in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false  
 }
 
 android {
