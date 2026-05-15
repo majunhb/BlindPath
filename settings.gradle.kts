72f3954616d22ac0b70278e1e539d94a152bce8d
majunhb <bojizhang@qq.com>
2026-05-15 12:31:18 +0800

    Add settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,22 +11,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        // 阿里云镜像仓库（包含高德地图等）
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
-        // JitPack
-        maven { url = uri("https://jitpack.io") }
-        // 百度仓库
-        maven { url = uri("https://dueros.baidu.com/maven") }
     }
 }
 
 rootProject.name = "BlindPath"
 include(":app")
 include(":base")
-include(":module_obstacle")
-include(":module_navigation")
-include(":module_voice")
-include(":module_settings")
-include(":module_community")
