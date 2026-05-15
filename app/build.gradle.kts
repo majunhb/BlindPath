d469bbeb20eef6c8cb185a6b91911c6661220e6a
majunhb <bojizhang@qq.com>
2026-05-15 17:25:40 +0800

    Update plugin versions in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.dagger.hilt.android")
-    id("com.google.devtools.ksp")
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
 }
+
 android {
     namespace = "com.blindpath.app"
     compileSdk = 34
