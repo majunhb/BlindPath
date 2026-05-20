b7aa0d0aa8913872c0cabfa4760d30bfe95b6458
majunhb <bojizhang@qq.com>
2026-05-20 16:08:39 +0800

    Update Hilt dependencies to use latest version
    Remove specific version numbers for Hilt dependencies.

--- module_indoor/build.gradle.kts
+++ module_indoor/build.gradle.kts
@@ -39,8 +39,8 @@ dependencies {
     implementation(project(":module_voice"))
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    ksp("com.google.dagger:hilt-android-compiler:2.48")
+    implementation("com.google.dagger:hilt-android:")
+    ksp("com.google.dagger:hilt-android-compiler:")
 
     // CameraX
     val cameraxVersion = "1.3.0"
