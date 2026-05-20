d5feeeb548502649f97183c1bf012f36006e5b5d
majunhb <bojizhang@qq.com>
2026-05-20 15:15:14 +0800

    Update lifecycle-runtime-ktx dependency
    Removed version number for lifecycle-runtime-ktx dependency.

--- module_voice/build.gradle.kts
+++ module_voice/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
 
     // Core
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
