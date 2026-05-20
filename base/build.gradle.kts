2df984322e29ae74d9ab1b249fbb2bb6a41af346
majunhb <bojizhang@qq.com>
2026-05-20 14:57:03 +0800

    Remove version numbers from Hilt dependenciesfix: upgrade Hilt 2.48 to 2.50 in base module
    Updated Hilt dependencies to remove version numbers.

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -44,8 +44,8 @@ android {
 
 dependencies {
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    ksp("com.google.dagger:hilt-android-compiler:2.48")
+    implementation("com.google.dagger:hilt-android:")
+    ksp("com.google.dagger:hilt-android-compiler:")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     // WorkManager + Hilt Worker
