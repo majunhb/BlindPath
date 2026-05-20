89545bc8d059453fdfa25a21e9703f5855b69417
majunhb <bojizhang@qq.com>
2026-05-20 15:03:38 +0800

    Change Hilt dependencies to dynamic versions
    Updated Hilt dependencies to use dynamic versions.

--- module_voice/build.gradle.kts
+++ module_voice/build.gradle.kts
@@ -37,8 +37,8 @@ dependencies {
     implementation(project(":base"))
     
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    ksp("com.google.dagger:hilt-android-compiler:2.48")
+    implementation("com.google.dagger:hilt-android:")
+    ksp("com.google.dagger:hilt-android-compiler:")
     
     // Timber
     implementation("com.jakewharton.timber:timber:5.0.1")
