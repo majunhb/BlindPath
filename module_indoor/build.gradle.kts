594e382847dad7014e372f088a59af10018009b4
majunhb <bojizhang@qq.com>
2026-05-21 12:21:19 +0800

    Update dependencies in build.gradle.kts

--- module_indoor/build.gradle.kts
+++ module_indoor/build.gradle.kts
@@ -36,41 +36,42 @@ android {
 dependencies {
     implementation(project(":base"))
     implementation(project(":module_obstacle"))
+    implementation(project(":module_voice"))
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.50")
     ksp("com.google.dagger:hilt-android-compiler:2.50")
 
+
     // CameraX
-    val cameraxVersion = "1.3.0"
-    implementation("androidx.camera:camera-core:$cameraxVersion")
-    implementation("androidx.camera:camera-camera2:$cameraxVersion")
-    implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
-    implementation("androidx.camera:camera-view:$cameraxVersion")
+    val cameraXVersion = "1.3.0"
+    implementation("androidx.camera:camera-core:$cameraXVersion")
+    implementation("androidx.camera:camera-camera2:$cameraXVersion")
+    implementation("androidx.camera:camera-lifecycle:$cameraXVersion")
+    implementation("androidx.camera:camera-view:$cameraXVersion")
 
     // ML Kit Image Labeling
     implementation("com.google.mlkit:image-labeling:17.0.7")
 
     // TensorFlow Lite
-    implementation("org.tensorflow:tensorflow-lite:")
-    implementation("org.tensorflow:tensorflow-lite-support:")
+    implementation("org.tensorflow:tensorflow-lite:2.14.0")
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
 
     // Timber
     implementation("com.jakewharton.timber:timber:5.0.1")
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.0")
 
     // Core
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 
     // Test dependencies
     testImplementation("junit:junit:4.13.2")
-    testImplementation("io.mockk:mockk:")
+    testImplementation("io.mockk:mockk:1.13.9")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
 }
-
